f49e8216958eee86a51bad06017108719ce86ac9
stixlink <stixlink@gmail.com>
2020-09-28 12:21:31 +0700

    refactoring

--- 0033_search-in-rotated-sorted-array/resolve.go
+++ 0033_search-in-rotated-sorted-array/resolve.go
@@ -1,20 +1,23 @@
 package _033_search_in_rotated_sorted_array
 
 func search(nums []int, target int) int {
+	if target == nums[0] {
+		return 0
+	}
 	if target < nums[0] {
 		for j := len(nums) - 1; j > 0; j-- {
 			if target == nums[j] {
 				return j
 			}
 		}
-	} else {
-		for g := 0; g < len(nums); g++ {
-			if target == nums[g] {
-				return g
-			}
+		return -1
+	}
+
+	for g := 0; g < len(nums); g++ {
+		if target == nums[g] {
+			return g
 		}
 	}
 
 	return -1
-
 }
